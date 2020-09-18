322c60f0394bab739a5a80e3693f4305b109aafa
coldstew <coldstew@gmail.com>
2020-09-19 00:49:11 +0900

    image assets added / draw bg

--- Source.cpp
+++ Source.cpp
@@ -4,12 +4,28 @@ sf::Vector2f viewSize(1024, 768);
 sf::VideoMode vm(viewSize.x, viewSize.y);
 sf::RenderWindow window(vm, "Hello SFMLGame !!!",sf::Style::Default);
 
+sf::Texture skyTexture;
+sf::Sprite skySprite;
+
+void init()
+{
+	skyTexture.loadFromFile("Assets/graphics/sky.png");
+	skySprite.setTexture(skyTexture);
+}
+
+void draw()
+{
+	window.draw(skySprite);
+}
+
 int main()
 {
-	//Init Game Objects
+	init();
+	
 	while (window.isOpen())
 	{
 		window.clear(sf::Color::Red);
+		draw();
 		window.display();
 	}
 	
